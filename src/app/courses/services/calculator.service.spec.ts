6bdb1cb4458669bffbac58b4f247837eaf31dc18
unknown <remigiuszwyciszkiewicz000@gmail.com>
2019-07-09 20:14:52 +0200

    beafore each

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,12 +1,33 @@
+import { CalculatorService } from './calculator.service';
+import { LoggerService } from './logger.service';
+
 describe('CalculatorService' , () => {
 
+  let calculator: CalculatorService, loggerSpy: any;
+
+  beforeEach(() => {
+    loggerSpy = jasmine.createSpyObj('loggerService', ['log']);
+   calculator = new CalculatorService(loggerSpy);
+  } );
+
 it('should add two numbers', () => {
-  pending();
+
+  // loggerSpy.log.and.returnValue();
+
+  const result = calculator.add(2.5, 2.5);
+
+  expect(result).toBe(5);
+  expect(loggerSpy.log).toHaveBeenCalledTimes(1);
 
 });
 
 it('should subtract two numbers', () => {
-  pending();
+
+  const result = calculator.subtract(10, 5);
+
+  expect(result).toBe(5, 'Unexpected subtraction result');
+  expect(loggerSpy.log).toHaveBeenCalledTimes(1);
+
 
 });
 
