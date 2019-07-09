b46fa4b515dcbdc25b09cccd0845c15ec5450110
unknown <remigiuszwyciszkiewicz000@gmail.com>
2019-07-09 20:29:22 +0200

    dependency injection

--- src/app/courses/services/calculator.service.spec.ts
+++ src/app/courses/services/calculator.service.spec.ts
@@ -1,13 +1,22 @@
 import { CalculatorService } from './calculator.service';
 import { LoggerService } from './logger.service';
+import { TestBed } from '@angular/core/testing';
 
 describe('CalculatorService' , () => {
 
   let calculator: CalculatorService, loggerSpy: any;
 
   beforeEach(() => {
-    loggerSpy = jasmine.createSpyObj('loggerService', ['log']);
-   calculator = new CalculatorService(loggerSpy);
+    loggerSpy = jasmine.createSpyObj('LoggerService', ['log']);
+
+    TestBed.configureTestingModule({
+      providers: [
+        CalculatorService,
+        {provide: LoggerService, useValue:  loggerSpy}
+      ]
+    });
+
+    calculator = TestBed.get(CalculatorService);
   } );
 
 it('should add two numbers', () => {
